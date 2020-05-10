f516bbc348ed7c037029828db61801ad7ddafd60
suulcoder <ef.proyectomundial@gmail.com>
2020-05-10 15:26:04 -0600

    structure and user jwt

---
+++ versus/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VersusConfig(AppConfig):
+    name = 'versus'
---
+++ viewedStory/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ViewedstoryConfig(AppConfig):
+    name = 'viewedStory'
