888f683333b08a6622770ac8f5ca887187450b11
suulcoder <ef.proyectomundial@gmail.com>
2020-05-24 04:29:05 -0600

    checkpoint of today2

--- vote/views.py
+++ vote/views.py
@@ -63,81 +63,72 @@ class VoteViewSet(viewsets.ModelViewSet):
         """
 
         while(hasToChangedOrder):                                                                   #hierarchy:
-            if(post.order==1):
-                hasToChangedOrder = False
-            currentPost = Post.objects.filter(topic=post.topic, order=post.order-1)[0]
-            if(currentPost.victories>post.victories):                                                                   #Victories
-                hasToChangedOrder = False
-            elif(currentPost.victories<post.victories):
-                currentPost.order+=1
-                post.order-=1
-                currentPost.save()
-                post.save()
-            else:
-                secondaryVictories_post = 0
-                for versus in Versus.objects.filter(post1=post):                                                        #Secondary Victories 
-                    for vote_ in Vote.objects.filter(versus=versus, winner=True):
-                        secondaryVictories_post += versus.post2.victories
-                for versus in Versus.objects.filter(post2=post):
-                    for vote_ in Vote.objects.filter(versus=versus, winner=False):
-                        secondaryVictories_post += versus.post1.victories
-                secondaryVictories_current = 0
-                for versus in Versus.objects.filter(post1=currentPost):
-                    for vote_ in Vote.objects.filter(versus=versus, winner=True):
-                        secondaryVictories_current += versus.post2.victories
-                for versus in Versus.objects.filter(post2=currentPost):
-                    for vote_ in Vote.objects.filter(versus=versus, winner=False):
-                        secondaryVictories_current += versus.post1.victories
-                if(secondaryVictories_current>secondaryVictories_post):
+            if(post.order!=1):
+                currentPost = Post.objects.filter(topic=post.topic, order=post.order-1)[0]
+                if(currentPost.victories>post.victories):                                                                   #Victories
                     hasToChangedOrder = False
-                elif(secondaryVictories_current<secondaryVictories_post):
+                elif(currentPost.victories<post.victories):
                     currentPost.order+=1
                     post.order-=1
                     currentPost.save()
                     post.save()
                 else:
-                    followerOnPost = 0
-                    for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):                  #Followers because of the Post
-                        followerOnPost += len(Follow.objects.filter(onVersus=versus))
-                    followerOnCurrent = 0
-                    for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):
-                        followerOnCurrent += len(Follow.objects.filter(onVersus=versus))
-                    if(followerOnCurrent>followerOnPost):
+                    secondaryVictories_post = 0
+                    for versus in Versus.objects.filter(post1=post):                                                        #Secondary Victories 
+                        for vote_ in Vote.objects.filter(versus=versus, winner=True):
+                            secondaryVictories_post += versus.post2.victories
+                    for versus in Versus.objects.filter(post2=post):
+                        for vote_ in Vote.objects.filter(versus=versus, winner=False):
+                            secondaryVictories_post += versus.post1.victories
+                    secondaryVictories_current = 0
+                    for versus in Versus.objects.filter(post1=currentPost):
+                        for vote_ in Vote.objects.filter(versus=versus, winner=True):
+                            secondaryVictories_current += versus.post2.victories
+                    for versus in Versus.objects.filter(post2=currentPost):
+                        for vote_ in Vote.objects.filter(versus=versus, winner=False):
+                            secondaryVictories_current += versus.post1.victories
+                    if(secondaryVictories_current>secondaryVictories_post):
                         hasToChangedOrder = False
-                    elif(followerOnCurrent<followerOnPost):
+                    elif(secondaryVictories_current<secondaryVictories_post):
                         currentPost.order+=1
                         post.order-=1
                         currentPost.save()
                         post.save()
                     else:
-                        shareOnPost = 0
-                        for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):                  #shares because of the Post
-                            shareOnPost += len(Share.objects.filter(versus=versus))
-                        shareOnCurrent = 0
+                        followerOnPost = 0
+                        for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):                  #Followers because of the Post
+                            followerOnPost += len(Follow.objects.filter(onVersus=versus))
+                        followerOnCurrent = 0
                         for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):
-                            shareOnCurrent += len(Share.objects.filter(versus=versus))
-                        if(shareOnCurrent>shareOnPost):
+                            followerOnCurrent += len(Follow.objects.filter(onVersus=versus))
+                        if(followerOnCurrent>followerOnPost):
                             hasToChangedOrder = False
-                        elif(shareOnCurrent<shareOnPost):
+                        elif(followerOnCurrent<followerOnPost):
                             currentPost.order+=1
                             post.order-=1
                             currentPost.save()
                             post.save()
                         else:
-                            likeOnPost = 0
-                            for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):               #Likes because of the Post
-                                likeOnPost += len(Like.objects.filter(versus=versus))
-                            likeOnCurrent = 0
+                            shareOnPost = 0
+                            for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):                  #shares because of the Post
+                                shareOnPost += len(Share.objects.filter(versus=versus))
+                            shareOnCurrent = 0
                             for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):
-                                likeOnCurrent += len(Like.objects.filter(versus=versus))
-                            if(likeOnCurrent>likeOnPost):
+                                shareOnCurrent += len(Share.objects.filter(versus=versus))
+                            if(shareOnCurrent>shareOnPost):
                                 hasToChangedOrder = False
-                            elif(likeOnCurrent<likeOnPost):
+                            elif(shareOnCurrent<shareOnPost):
                                 currentPost.order+=1
                                 post.order-=1
                                 currentPost.save()
                                 post.save()
                             else:
+                                likeOnPost = 0
+                                for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):               #Likes because of the Post
+                                    likeOnPost += len(Like.objects.filter(versus=versus))
+                                likeOnCurrent = 0
+                                for versus in Versus.objects.filter(Q(post1=post) | Q(post2=post)):
+                                    likeOnCurrent += len(Like.objects.filter(versus=versus))
                                 if(likeOnCurrent>likeOnPost):
                                     hasToChangedOrder = False
                                 elif(likeOnCurrent<likeOnPost):
@@ -146,13 +137,23 @@ class VoteViewSet(viewsets.ModelViewSet):
                                     currentPost.save()
                                     post.save()
                                 else:
-                                    pick = random.randint(0,1)                                                                  #Random 
-                                    if(pick==0):
+                                    if(likeOnCurrent>likeOnPost):
                                         hasToChangedOrder = False
-                                    elif(pick==1):
+                                    elif(likeOnCurrent<likeOnPost):
                                         currentPost.order+=1
                                         post.order-=1
                                         currentPost.save()
                                         post.save()
-                                    hasToChangedOrder = False                  
+                                    else:
+                                        pick = random.randint(0,1)                                                                  #Random 
+                                        if(pick==0):
+                                            hasToChangedOrder = False
+                                        elif(pick==1):
+                                            currentPost.order+=1
+                                            post.order-=1
+                                            currentPost.save()
+                                            post.save()
+                                        hasToChangedOrder = False
+            else:
+                hasToChangedOrder = False                  
         return Response(serializer.data)
